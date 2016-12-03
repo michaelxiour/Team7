8f9c13562be2dabaacc9c64f6dc47e66a71d15aa
michael <michael@DESKTOP-QPJ1CHT.home>
2016-12-03 13:17:53 +0000

    Code 2

---
+++ Groupproject-android/src/com/rtsgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rtsgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rtsgame.RtsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RtsGame(), config);
+	}
+}
